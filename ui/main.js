a6db762dc7435b4055671a8c7f8ece735986f9ed
divyadevan <30824768+divyadevan@users.noreply.github.com>
2017-08-12 14:27:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,14 +11,14 @@ var img=document.getElementById('madi');
 var marginLeft=0;
 function moveRight()
 {
-    marginLeft=marginLeft + 10;
+    marginLeft=marginLeft + 5;
     img.style.marginLeft=marginLeft+'px';
     
     
 }
 img.onclick = function () {
     //img.style.marginLeft='100px';
-     var interval=setInterval(moveRight,100);
+     var interval=setInterval(moveRight,50);
 };
    
 
