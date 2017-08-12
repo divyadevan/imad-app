1f48dc243f6e897a2b816c830f09d03db6f57031
divyadevan <30824768+divyadevan@users.noreply.github.com>
2017-08-12 13:50:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,3 +7,8 @@ var element = document.getElementById('main-text');
 
 
 element.innerHTML='New Value';
+var img=document.getElementById('img');
+img.onClick=function(){
+img.style.margineLeft='100px';
+
+}
