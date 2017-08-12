caf23b3c47f2d3419f47d3df9e0c693c3ad8933a
divyadevan <30824768+divyadevan@users.noreply.github.com>
2017-08-12 14:26:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,18 +8,19 @@ var element = document.getElementById('main-text');
 
 element.innerHTML='New Value';
 var img=document.getElementById('madi');
-//var marginLeft=0;
-//function moveRight()
-//{
-   // marginLeft=marginLeft + 10;
-   // img.style.marginLeft=marginLeft+'px';
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft + 10;
+    img.style.marginLeft=marginLeft+'px';
     
     
-//}
+}
 img.onclick = function () {
-    img.style.marginLeft='100px';
+    //img.style.marginLeft='100px';
+     var interval=setInterval(moveRight,100);
 };
-    //var interval=setInterval(moveRight,100);
+   
 
 
 
