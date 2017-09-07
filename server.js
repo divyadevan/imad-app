a889ff269de0500caf86fd3d45cdc9597f551aa2
divyadevan <30824768+divyadevan@users.noreply.github.com>
2017-09-07 19:04:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var crypto=require('crypto');
 var app = express();
 app.use(morgan('combined'));
 
@@ -17,6 +17,28 @@ app.get('/counter', function (req, res) {
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
+
+function hash(input)
+{
+    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+    
+}
+    
+
+
+
+
+app.get('/hash/:input', function (req, res) {
+
+var hashedString=hash(req.params.input,'this-is-some-random-string');
+res.send(hashedString);
+
+
+
+});
+
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
